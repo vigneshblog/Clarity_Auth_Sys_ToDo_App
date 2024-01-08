864b0c3e0b0afe97e4d5ec7429e4812728b877b2
Vignesh Sankaran <vignesh@altsystech.com>
2024-01-08 20:49:49 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'clarity_auth_syst_todo_list_app';
+}
